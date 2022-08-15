bfa9148b027a8132740b84e92cbf4c5983ec6e16
Nettester <github@gluch.eu>
2022-08-15 15:40:34 +0200

    Co-authored-by: Cemil Kerimoglu <cemil.a.kerimoglu@gmail.com> Co-authored-by: Eunnylans <Eunnylans@users.noreply.github.com> Co-authored-by: claurennt <claurennt@users.noreply.github.com>

--- src/Components/Control.js
+++ src/Components/Control.js
@@ -1,17 +1,50 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faPlay, faPause, faForward, faBackward} from "@fortawesome/free-solid-svg-icons";
+// import Audio from 'ts-audio';
+// import Lazarus from './music/Lazarus.mp3';
 
 const Control = (props) => {
+
+  const [isPlaying, setIsplaying] = useState(false);
+  const [picture, setPicture] = useState();
+
+  // let audio = new Audio(`/Music/${props.songs[0].file}`)
+
+  const audio = useRef(new Audio(`/Music/${props.songs[0].file}`));
+
+
+  const handlePlay = () => {
+    setIsplaying(!isPlaying);
+    console.log(props.songs);
+    !isPlaying ? audio.current.play() : audio.current.pause();
+  }
+
+  /*
+  const audio = Audio({
+    file: Lazarus
+  })
+
+  const play = () => {
+    audio.play()
+  }
+
+    const pause = () => {
+    audio.pause()
+  }
+  */
+
   return (
     <div className="music-player-controls">
+        <img src={`/Images/${picture}`} />
+        {console.log(picture)}
         <button className="shuffle-btn" onClick={props.moveBackwards}> 
             <FontAwesomeIcon icon={faBackward} />
         </button>
-        <button className="playPause-btn" onClick={() =>props.setIsPlaying(!props.isPlaying)}> 
-            <FontAwesomeIcon icon={props.isPlaying ? faPause : faPlay} />
+        <button className="playPause-btn" onClick={handlePlay}> 
+            <FontAwesomeIcon icon={isPlaying ? faPause : faPlay} />
         </button>
-        <button className="shuffle-btn" onClick={()=> props.handleShuffle()}>
+        <button className="shuffle-btn" onClick={()=> props.handleShuffle}>
             <FontAwesomeIcon icon={faForward} />
         </button>
     </div>
--- src/Components/Music.js
+++ src/Components/Music.js
@@ -7,17 +7,17 @@ const Music = ( { songs } ) => {
     const [songHistory, setSongHistory] = useState([]) 
     const [currentSong, setCurrentSong] = useState(
         {
-            id: Number,
-            picture: String,
-            musicTitle: String,
-            // time: Number,
-            file: String
+            id: 1,
+            artist: "Beyoncé",
+            picture: "Beyonce.png",
+            musicTitle: "Beyonce - Halo",
+            file: "Beyonce_-_Halo.m4a"
         }
     );
-    const [isPlaying, setIsplaying] = useState(false);
+    // const [isPlaying, setIsplaying] = useState(false);
 
     const randomNumber = () => (Math.floor(Math.random() * 5) + 1); 
-    setCurrentSong(songs.filter((song) => randomNumber()===song.id));
+   // setCurrentSong(songs.filter((song) => randomNumber()===song.id));
 
     const moveBackwards = () => {
         setCurrentSong(songHistory[(songHistory.length - 2)]);
@@ -27,15 +27,16 @@ const Music = ( { songs } ) => {
         setCurrentSong(songs.filter((song) => randomNumber()===song.id));  
         setSongHistory((prev) => [...prev, currentSong]);
     }
-
+console.log("test",typeof currentSong?.picture)
     return (
         <div>
             <h1>Music App</h1>
-            <img src={currentSong?.picture} alt="picture of song"/> 
-            <link href={currentSong?.file}> {currentSong?.musicTitle} </link>
+            {console.log(currentSong.picture)}
+            <img src={`./Images/${currentSong.picture}`} alt={currentSong.picture} /> 
+            <a href={currentSong.file}> {currentSong.musicTitle} </a>
             {/* Where is this props coming from? */}
-            <ArtistInfo id={props.id}/>
-            <Control isPlaying={isPlaying} moveBackwards={moveBackwards} handleShuffle={handleShuffle} />
+            {/* <ArtistInfo id={songs[(songHistory.length- 1)].id} picture_src={}/> */}
+            <Control songs={songs} moveBackwards={moveBackwards} handleShuffle={handleShuffle} />
             
         </div>
     )
